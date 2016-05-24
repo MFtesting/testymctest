db78c1bc0d2dcb58027027f24596b2319c7b023b
carnal0wnage <chris@carnal0wnage.com>
2016-05-24 16:52:46 -0400

    try multi os

--- modules/downloadfromurl.py
+++ modules/downloadfromurl.py
@@ -6,16 +6,18 @@ import platform
 #download to temp for osx
 
 def run(**args):
-    url = "http://localhost:8000/meh.txt"
+    url = "http://172.16.153.1:8000/meh.txt"
     print "[*] In downoadfromurl module."
     
     if platform.system() == "linux" or platform.system() == "linux2":
-        print ("its linux") #LINUX
-    elif platform.system() == "darwin":
-        print ("its OSX") #OSX
+        print ("it's Linux") #LINUX #untested
         urllib.urlretrieve(url, os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
         info = os.path.getsize(os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
-   elif platform.system() == "win32":
+    elif platform.system() == "darwin" or platform.system() == "Darwin":
+        print ("it's OSX") #OSX
+        urllib.urlretrieve(url, os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
+        info = os.path.getsize(os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
+    elif platform.system() == "win32" or platform.system() == "Windows":
         print ("it's Windows") # Windows
         urllib.urlretrieve(self.url, os.path.join(os.getenv('TEMP') + '\\'  + self.url.split('/')[-1]))
         info = os.path.getsize(os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
