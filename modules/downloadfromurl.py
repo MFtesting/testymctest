7039188fed58fe3981396d80c13d3c82916cb6ab
carnal0wnage <chris@carnal0wnage.com>
2016-05-24 16:49:54 -0400

    try multi os

--- modules/downloadfromurl.py
+++ modules/downloadfromurl.py
@@ -1,13 +1,22 @@
 import urllib
 import tempfile
 import os
+import platform
 
 #download to temp for osx
 
 def run(**args):
     url = "http://localhost:8000/meh.txt"
     print "[*] In downoadfromurl module."
-
-    urllib.urlretrieve(url, os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
-    info = os.path.getsize(os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
+    
+    if platform.system() == "linux" or platform.system() == "linux2":
+        print ("its linux") #LINUX
+    elif platform.system() == "darwin":
+        print ("its OSX") #OSX
+        urllib.urlretrieve(url, os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
+        info = os.path.getsize(os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
+   elif platform.system() == "win32":
+        print ("it's Windows") # Windows
+        urllib.urlretrieve(self.url, os.path.join(os.getenv('TEMP') + '\\'  + self.url.split('/')[-1]))
+        info = os.path.getsize(os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
     return str("downloaded file of %s bytes" %info)
