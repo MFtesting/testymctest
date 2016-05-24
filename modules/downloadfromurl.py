2decedb77d127bfa8a2f05355a13701bc83addba
carnal0wnage <chris@carnal0wnage.com>
2016-05-24 09:52:56 -0400

    works for osx

--- modules/downloadfromurl.py
+++ modules/downloadfromurl.py
@@ -1,14 +1,13 @@
-import urllib2
-import json
+import urllib
+import tempfile
 import os
 
-#download to temp
+#download to temp for osx
 
 def run(**args):
     url = "http://localhost:8000/meh.txt"
     print "[*] In downoadfromurl module."
-    meh = urllib.urlretrieve(url, os.path.join(os.getenv('TEMP') + '\\'  + url.split('/')[-1]))
-    print meh
 
-    return ("something")
-
+    urllib.urlretrieve(url, os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
+    info = os.path.getsize(os.path.join(tempfile.gettempdir() + '/'  + url.split('/')[-1]))
+    return str("downloaded file of %s bytes" %info)
