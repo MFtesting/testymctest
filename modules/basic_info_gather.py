1151fd6aeca15d5c65f040e41bbfc70f686503ed
carnal0wnage <chris@carnal0wnage.com>
2016-05-24 16:37:58 -0400

    typo

--- modules/basic_info_gather.py
+++ modules/basic_info_gather.py
@@ -9,15 +9,15 @@ def run(**args):
     if platform.system() == "linux" or platform.system() == "linux2":
         print ("its linux") #LINUX
         info = "Linux \n"
-        info += os.environ
+        info += str(os.environ)
     elif platform.system() == "darwin" or platform.system() == "Darwin":
         print ("it's OSX") #OSX
         info = "OSX \n"
-        info += os.environ
+        info += str(os.environ)
     elif platform.system() == "win32":
         print ("it's Windows") # Windows
         info = "Windows \n"
-        info += os.environ
+        info += sr(os.environ)
     return str(info)
 
 
