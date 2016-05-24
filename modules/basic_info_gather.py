394f0c64fd37a620f102feffd01faf791d7d0388
carnal0wnage <chris@carnal0wnage.com>
2016-05-24 16:21:31 -0400

    more

---
+++ modules/basic_info_gather.py
@@ -0,0 +1,24 @@
+import os
+import platform
+
+def run(**args):
+    print "[*] In basic info gather module."
+    
+    if platform.system() == "linux" or platform.system() == "linux2":
+        print ("its linux") #LINUX
+        info = "Linux \n"
+        info = os.environ
+    elif platform.system() == "darwin":
+        print ("it's OSX") #OSX
+        info = "OSX \n"
+        info = os.environ
+    elif platform.system() == "win32":
+        print ("it's Windows") # Windows
+        info = "Windows \n"
+        info =+ os.environ
+    return str("downloaded file of %s bytes" %info)
+
+    return str(info)
+
+
+
