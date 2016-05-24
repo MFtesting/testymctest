509107c39232517224e7577e2a7bba5ad5ef5e34
carnal0wnage <chris@carnal0wnage.com>
2016-05-24 08:57:09 -0400

    geolocate

--- modules/geolocate.py
+++ modules/geolocate.py
@@ -1,17 +1,15 @@
-import os
+import urllib2
+import json
 
 def run(**args):
     print "[*] In geolocate module."
-    try:
-        req = urllib2.Request('http://ip-api.com/json/', data=None, headers={
+    req = urllib2.Request('http://ip-api.com/json/', data=None, headers={
                               'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36' #chrome 50.0 on Win7
                               })
-        response = urllib2.urlopen(req)
-        if response.code == 200:
-            encoding = response.headers.getparam('charset')
-            return json.loads(response.read().decode(encoding))
-        else:
-            return ("failed")
-    except Exception:
-        return str("Failed Exception")
+    response = urllib2.urlopen(req)
+    if response.code == 200:
+        encoding = response.headers.getparam('charset')
+        return json.loads(response.read().decode(encoding))
+    else:
+        return ("failed")
 
