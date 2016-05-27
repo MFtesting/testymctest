b7c4adb11d3280e1842caa9718ffb552392050ea
carnal0wnage <chris@carnal0wnage.com>
2016-05-26 21:59:06 -0400

    meh

--- modules/basic_info_gather.py
+++ modules/basic_info_gather.py
@@ -10,17 +10,17 @@ def run(**args):
     if platform.system() == "linux" or platform.system() == "linux2":
         print ("Its linux") #LINUX
         info = "Linux \n"
-        info += str("Timezone: %s \n" % time.tzname)
+        info += "Timezone: " + str(time.tzname) +"\n"
         info += str(os.environ)
     elif platform.system() == "darwin" or platform.system() == "Darwin":
         print ("It's OSX") #OSX
         info = "OSX \n"
-        info += str("Timezone: %s \n" ,(%time.tzname))
+        info += "Timezone: " + str(time.tzname) +"\n"
         info += str(os.environ)
     elif platform.system() == "win32" or platform.system() == "Windows":
         print ("It's Windows") # Windows
         info = "Windows \n"
-        info += str("Timezone: %s \n" % time.tzname)
+        info += "Timezone: " + str(time.tzname) +"\n"
         info += str(os.environ)
     return str(info)
 
