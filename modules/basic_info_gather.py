6b696366c6f31520362fecf8e9c73bfb056ea6d5
carnal0wnage <chris@carnal0wnage.com>
2016-05-26 21:36:01 -0400

    add timezone to info gather

--- modules/basic_info_gather.py
+++ modules/basic_info_gather.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import time
 
 def run(**args):
     print "[*] In basic info gather module."
@@ -9,14 +10,17 @@ def run(**args):
     if platform.system() == "linux" or platform.system() == "linux2":
         print ("its linux") #LINUX
         info = "Linux \n"
+        info += ("Timezone: %s \n" %time.tzname)
         info += str(os.environ)
     elif platform.system() == "darwin" or platform.system() == "Darwin":
         print ("it's OSX") #OSX
         info = "OSX \n"
+        info += ("Timezone: %s \n" %time.tzname)
         info += str(os.environ)
     elif platform.system() == "win32" or platform.system() == "Windows":
         print ("it's Windows") # Windows
         info = "Windows \n"
+        info += ("Timezone: %s \n" %time.tzname)
         info += str(os.environ)
     return str(info)
 
