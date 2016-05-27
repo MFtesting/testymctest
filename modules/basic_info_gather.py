6ed597176ed4885d38ff0218511244db19032f21
carnal0wnage <chris@carnal0wnage.com>
2016-05-26 22:03:39 -0400

    meh

--- modules/basic_info_gather.py
+++ modules/basic_info_gather.py
@@ -9,18 +9,21 @@ def run(**args):
     
     if platform.system() == "linux" or platform.system() == "linux2":
         print ("Its linux") #LINUX
-        info = "Linux \n"
+        info = "OS Type: Linux \n"
         info += "Timezone: " + str(time.tzname) +"\n"
+        info += "Hostname: " + str(socket.gethostname()) +"\n"
         info += str(os.environ)
     elif platform.system() == "darwin" or platform.system() == "Darwin":
         print ("It's OSX") #OSX
-        info = "OSX \n"
+        info = "OS Type: OSX \n"
         info += "Timezone: " + str(time.tzname) +"\n"
+        info += "Hostname: " + str(socket.gethostname()) +"\n"
         info += str(os.environ)
     elif platform.system() == "win32" or platform.system() == "Windows":
         print ("It's Windows") # Windows
-        info = "Windows \n"
+        info = "OS Type: Windows \n"
         info += "Timezone: " + str(time.tzname) +"\n"
+        info += "Hostname: " + str(socket.gethostname()) +"\n"
         info += str(os.environ)
     return str(info)
 
