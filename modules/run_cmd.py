5d400bbdb8d1f614fbf67b77b8e27c882cfdc4de
carnal0wnage <chris@carnal0wnage.com>
2016-05-26 22:37:51 -0400

    runcmd

---
+++ modules/run_cmd.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+def run(**args):
+    cmd = "ipconfig /all"
+    print "[*] In run command module."
+    # issue the shell command we want
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # read out the data of stdout
+    data = proc.stdout.read() + proc.stderr.read()
+    return str(data)
