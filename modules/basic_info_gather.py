a85ee467e1a77b882ec153e999991bff5a82bf93
carnal0wnage <chris@carnal0wnage.com>
2016-05-26 22:18:53 -0400

    more infoz

--- modules/basic_info_gather.py
+++ modules/basic_info_gather.py
@@ -11,21 +11,25 @@ def run(**args):
     if platform.system() == "linux" or platform.system() == "linux2":
         print ("Its linux") #LINUX
         info = "OS Type: Linux \n"
-        info += "Timezone: " + str(time.tzname) +"\n"
         info += "Hostname: " + str(socket.gethostname()) +"\n"
-        info += str(os.environ)
+        info += "Username: " + str(os.getenv('USER')) +"\n"
+        info += "Timezone: " + str(time.tzname) +"\n"
+        info += "Timezone: " + str(os.getenv('USER')) +"\n"
+        info += "Environment" + str(os.environ)
     elif platform.system() == "darwin" or platform.system() == "Darwin":
         print ("It's OSX") #OSX
         info = "OS Type: OSX \n"
-        info += "Timezone: " + str(time.tzname) +"\n"
         info += "Hostname: " + str(socket.gethostname()) +"\n"
-        info += str(os.environ)
+        info += "Username: " + str(os.getenv('USER')) +"\n"
+        info += "Timezone: " + str(time.tzname) +"\n"
+        info += "Environment" + str(os.environ)
     elif platform.system() == "win32" or platform.system() == "Windows":
         print ("It's Windows") # Windows
         info = "OS Type: Windows \n"
-        info += "Timezone: " + str(time.tzname) +"\n"
         info += "Hostname: " + str(socket.gethostname()) +"\n"
-        info += str(os.environ)
+        info += "Timezone: " + str(time.tzname) +"\n"
+        info += "Username: " + str(os.getenv('USERNAME')) +"\n"
+        info += "Environment" + str(os.environ)
     return str(info)
 
 
