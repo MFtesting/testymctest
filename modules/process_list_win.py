fe00d51795f6a013680e49c5e5b513b50ec95d64
carnal0wnage <chris@carnal0wnage.com>
2016-05-28 00:09:27 -0400

    .

--- modules/process_list_win.py
+++ modules/process_list_win.py
@@ -6,16 +6,16 @@ import pythoncom
 
 
 
-    def run(**args):
-        try:
-            print "[*] In Process List Module."
-            pythoncom.CoInitialize()
-            data = ""
-            w = wmi.WMI ()
-            data += "PID \tPPID \tNAME \n"
-            for process in w.Win32_Process ():
+def run(**args):
+    try:
+        print "[*] In Process List Module."
+        pythoncom.CoInitialize()
+        data = ""
+        w = wmi.WMI ()
+        data += "PID \tPPID \tNAME \n"
+        for process in w.Win32_Process ():
                 data += str(process.ProcessId) + "\t" + str(process.ParentProcessId) +"\t" + str(process.Name) +"\n"
-            return str(data)
-        except Exception as e:
-            print e
-            return str("e")
+        return str(data)
+    except Exception as e:
+        print e
+        return str("e")
