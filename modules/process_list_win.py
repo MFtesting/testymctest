8bb9037577ecf1cdefbbedb27f142fecd5c5e688
carnal0wnage <chris@carnal0wnage.com>
2016-05-28 00:01:37 -0400

    .

--- modules/process_list_win.py
+++ modules/process_list_win.py
@@ -9,16 +9,17 @@ except ImportError:
     except ImportError:
 
 
-    def run(**args):
-        try:
-            print "[*] In Process List Module."
-            pythoncom.CoInitialize()
-            data = ""
-            w = wmi.WMI ()
-            data += "PID \tPPID \tNAME \n"
-            for process in w.Win32_Process ():
-                data += str(process.ProcessId) + "\t" + str(process.ParentProcessId) +"\t" + str(process.Name) +"\n"
-            return str(data)
-    except Exception as e:
-        print e
-        return str("e")
+        def run(**args):
+            try:
+                print "[*] In Process List Module."
+                pythoncom.CoInitialize()
+                data = ""
+                w = wmi.WMI ()
+                data += "PID \tPPID \tNAME \n"
+                for process in w.Win32_Process ():
+                    data += str(process.ProcessId) + "\t" + str(process.ParentProcessId) +"\t" + str(process.Name) +"\n"
+                return str(data)
+            except Exception as e:
+                print e
+                return str("e")
+run()
