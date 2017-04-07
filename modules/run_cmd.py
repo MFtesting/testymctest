e06b3efdad5792d95d4a51315ead5798cbafaf7f
MFtesting <testing@carnal0wnage.com>
2017-04-07 17:51:00 -0400

    Update run_cmd.py

--- modules/run_cmd.py
+++ modules/run_cmd.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def run(**args):
-    cmd = "ipconfig /all"
+    cmd = "ifconfig"
     print "[*] In run command module."
     # issue the shell command we want
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
